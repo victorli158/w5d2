3cedb97688fffc6920118d4b140796602681fa44
Victor Li <victor.li158@gmail.com>
2017-04-25 15:38:45 -0700

    Complete P02

--- w5d2/skeleton/lib/controller_base.rb
+++ w5d2/skeleton/lib/controller_base.rb
@@ -8,25 +8,43 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise "double render error" if already_built_response?
+    @res.status = 302
+    @res.location = url
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "double render error" if already_built_response?
+
+    @res['Content-Type'] = content_type
+    @res.body = [content]
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    raise "double render error" if already_built_response?
+
+    
+    @already_built_response = true
+    binding
   end
 
   # method exposing a `Session` object
@@ -35,6 +53,6 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+
   end
 end
-
