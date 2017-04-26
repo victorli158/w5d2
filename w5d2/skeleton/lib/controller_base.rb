13830c1388a405a7e800bb691900793c3805b1fb
Victor Li <victor.li158@gmail.com>
2017-04-25 17:00:11 -0700

    Complete P03 and P04

--- w5d2/skeleton/lib/controller_base.rb
+++ w5d2/skeleton/lib/controller_base.rb
@@ -1,5 +1,6 @@
 require 'active_support'
 require 'active_support/core_ext'
+require 'active_support/inflector'
 require 'erb'
 require_relative './session'
 
@@ -7,9 +8,10 @@ class ControllerBase
   attr_reader :req, :res, :params
 
   # Setup the controller
-  def initialize(req, res)
+  def initialize(req, res, route_params = {})
     @req = req
     @res = res
+    @params = @params = route_params.merge(req.params)
     @already_built_response = false
   end
 
@@ -24,6 +26,8 @@ class ControllerBase
     @res.status = 302
     @res.location = url
     @already_built_response = true
+
+    session.store_session(@res)
   end
 
   # Populate the response with content.
@@ -35,24 +39,38 @@ class ControllerBase
     @res['Content-Type'] = content_type
     @res.body = [content]
     @already_built_response = true
+
+    session.store_session(@res)
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
-    raise "double render error" if already_built_response?
+    dir_path = File.dirname(__FILE__)
+    template_fname = File.join(
+      dir_path, "..",
+      "views", self.class.name.underscore, "#{template_name}.html.erb"
+    )
 
-    
-    @already_built_response = true
-    binding
+    template_code = File.read(template_fname)
+
+    render_content(
+      ERB.new(template_code).result(binding),
+      "text/html"
+    )
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
 
+  end
+
+  def flash
+    @flash ||= Flash.new(@req)
   end
 end
